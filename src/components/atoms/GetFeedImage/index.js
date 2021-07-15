a85832de7945c96249f32fd8361e2ea6f3cac136
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-07-15 12:36:47 +0100

    BusMap

--- src/components/atoms/GetFeedImage/index.js
+++ src/components/atoms/GetFeedImage/index.js
@@ -3,16 +3,9 @@ import { StaticImage } from "gatsby-plugin-image";
 import styles from "./GetFeedImage.module.css";
 export default function GetFeedImage({ option }) {
   const [camera1, setCamera1] = useState("");
-  console.log("🚀 ~ file: index.js ~ line 5 ~ GetFeedImage ~ camera1", camera1);
   const [camera2, setCamera2] = useState("");
-  console.log("🚀 ~ file: index.js ~ line 7 ~ GetFeedImage ~ camera2", camera2);
   const [camera3, setCamera3] = useState("");
-  console.log("🚀 ~ file: index.js ~ line 9 ~ GetFeedImage ~ camera3", camera3);
   const [camera4, setCamera4] = useState("");
-  console.log(
-    "🚀 ~ file: index.js ~ line 11 ~ GetFeedImage ~ camera4",
-    camera4
-  );
 
   useEffect(() => {
     async function fetchFeedImages() {
@@ -52,36 +45,36 @@ export default function GetFeedImage({ option }) {
           console.log(error);
         });
 
-      console.log("test1");
+      // console.log("test1");
 
-      fetch(
-        `https://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/NE1+8XG/limit/10/
-          `,
-        {
-          mode: "no-cors",
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-          },
-        }
-      )
-        // .then((response) => {response.json(); console.log(response)})
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(
-            "🚀 ~ file: index.js ~ line 29 ~ .then ~ response",
-            response
-          );
-          console.log(
-            "🚀 ~ file: index.js ~ line 29 ~ .then ~ response",
-            response
-          );
-          console.log(response);
-          console.log("test2");
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      console.log("test3");
+      // fetch(
+      //   `https://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/NE1+8XG/limit/10/
+      //     `,
+      //   {
+      //     mode: "no-cors",
+      //     headers: {
+      //       "Access-Control-Allow-Origin": "*",
+      //     },
+      //   }
+      // )
+      //   // .then((response) => {response.json(); console.log(response)})
+      //   .then((response) => response.json())
+      //   .then((response) => {
+      //     console.log(
+      //       "🚀 ~ file: index.js ~ line 29 ~ .then ~ response",
+      //       response
+      //     );
+      //     console.log(
+      //       "🚀 ~ file: index.js ~ line 29 ~ .then ~ response",
+      //       response
+      //     );
+      //     console.log(response);
+      //     console.log("test2");
+      //   })
+      //   .catch((error) => {
+      //     console.log(error);
+      //   });
+      // console.log("test3");
     }
     fetchFeedImages();
   }, []);
---
+++ src/components/molecules/BusMap/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "./BusMap.module.css";
+
+import BusesData from "../../atoms/BusesData";
+import BusDataExample from "../../atoms/BusesData/BusDataExample.json";
+import BusIcon from "../../../images/BusBirdsEye.png";
+import car from "../../../assets/icons/graphic_car.svg";
+import face from "../../../images/smiley-face2.jpg";
+
+
+
+// get current location optional
+// rotate bus to bounding or create an arrow (https://www.npmjs.com/package/leaflet-marker-rotation)(https://codesandbox.io/s/9hrd3?file=/main.js)
+// hover to find bus details or have a marker that shows bus no (show buses or just numbers)
+// only show buses within distance (hook) https://leafletjs.com/reference-1.7.1.html#latlng-distanceto
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function BusMap() {
+  console.log("🚀 ~ file: index.js ~ line 6 ~ BusesData", BusDataExample);
+
+  // delete L.Icon.Default.prototype._getIconUrl;
+
+  // L.Icon.Default.mergeOptions({
+  //   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  //   iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  //   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+  // });
+  console.log(Marker);
+  const customMarker = L.icon({
+    iconUrl: BusIcon,
+    iconSize: [33, 100],
+  });
+
+  return (
+    <MapContainer center={[54.97206769445005, -1.6132124536205563]} zoom={14}>
+      <TileLayer
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+        attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ"
+      />
+
+      {BusDataExample.map((bus) => (
+        <>
+          <Marker
+            icon={customMarker}
+            rotationAngle={'45'}
+            key={Math.floor(Math.random() * 999999999999)}
+            position={[
+              bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation
+                .Latitude,
+              bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation
+                .Longitude,
+            ]}
+          >
+            <Popup></Popup>
+          </Marker>
+        </>
+      ))}
+    </MapContainer>
+  );
+}
