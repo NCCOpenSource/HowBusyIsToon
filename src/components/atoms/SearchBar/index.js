1dbc11e5dbd8753bb62ba119425da5d7513f1c39
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-07-19 11:58:29 +0100

    busmap search and filter

---
+++ src/components/atoms/SearchBar/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./SearchBar.module.css";
+export default function SearchBar() {
+  return (
+    <input type="text" placeholder="Search Bus's" className={styles.search} />
+  );
+}
--- src/components/molecules/BusMap/index.js
+++ src/components/molecules/BusMap/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
 import L from "leaflet";
-import "./BusMap.module.css";
+import styles from "./BusMap.module.css";
 import "./busMap.css";
 import BusesData from "../../atoms/BusesData";
 import BusDataExample from "../../atoms/BusesData/BusDataExample.json";
@@ -13,6 +13,7 @@ import average from "../../../images/map-marker-average.png";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import "leaflet/dist/leaflet.css";
 import SelectSearch from "react-select-search";
+import SearchField from "react-search-field";
 
 // get current location optional
 // rotate bus to bounding or create an arrow (https://www.npmjs.com/package/leaflet-marker-rotation)(https://codesandbox.io/s/9hrd3?file=/main.js)
@@ -20,6 +21,7 @@ import SelectSearch from "react-select-search";
 // only show buses within distance (hook) https://leafletjs.com/reference-1.7.1.html#latlng-distanceto
 
 export default function BusMap() {
+  const [input, setInput] = useState("11");
   console.log("🚀 ~ file: index.js ~ line 8 ~ BusDataExample", BusDataExample);
 
   const customMarker = L.icon({
@@ -41,38 +43,55 @@ export default function BusMap() {
     });
     return marker;
   }
+  const filteredBuses = BusDataExample.filter((bus) =>
+    bus.VehicleActivity.MonitoredVehicleJourney.LineRef.includes(input)
+  );
+  console.log(
+    "🚀 ~ file: index.js ~ line 49 ~ BusMap ~ filteredBuses",
+    filteredBuses
+  );
+
+  function onChange(event) {
+    setInput(event.target.value);
+  }
 
   return (
     <>
-  
-    <MapContainer center={[54.97206769445005, -1.6132124536205563]} zoom={14}>
-    
-      <TileLayer
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+      <input
+        type="text"
+        id="header-search"
+        placeholder="Search bus number"
+        name="s"
+        onChange={onChange}
       />
-      {/* <MarkerClusterGroup > */}
+      <MapContainer center={[54.97206769445005, -1.6132124536205563]} zoom={14}>
+        
+        {" "}
+        <TileLayer
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        />
+        {/* <MarkerClusterGroup > */}
+        {filteredBuses.map((bus) => {
+          const seatsavailable =
+            bus.VehicleActivity.Extensions.VehicleJourney.SeatedCapacity -
+            bus.VehicleActivity.Extensions.VehicleJourney.SeatedOccupancy;
 
-      {BusDataExample.map((bus) => {
-        const seatsavailable =
-          bus.VehicleActivity.Extensions.VehicleJourney.SeatedCapacity -
-          bus.VehicleActivity.Extensions.VehicleJourney.SeatedOccupancy;
+          const WheelChaireseatsavailable =
+            bus.VehicleActivity.Extensions.VehicleJourney.WheelchairCapacity -
+            bus.VehicleActivity.Extensions.VehicleJourney.WheelchairOccupancy;
 
-        const WheelChaireseatsavailable =
-          bus.VehicleActivity.Extensions.VehicleJourney.WheelchairCapacity -
-          bus.VehicleActivity.Extensions.VehicleJourney.WheelchairOccupancy;
-
-        if (
-          bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation.Latitude <
-            55.02255 &&
-          bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation.Latitude >
-            54.92255 &&
-          bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation
-            .Longitude < -1.5132124536205563 &&
-          bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation
-            .Longitude > -1.7132124536205563
-        ) {
-          return (
+          if (
+            bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation
+              .Latitude < 55.02255 &&
+            bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation
+              .Latitude > 54.92255 &&
+            bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation
+              .Longitude < -1.5132124536205563 &&
+            bus.VehicleActivity.MonitoredVehicleJourney.VehicleLocation
+              .Longitude > -1.7132124536205563
+          ) {
+            return (
               <Marker
                 rotationAngle={180}
                 rotationOrigin={"center"}
@@ -109,12 +128,11 @@ export default function BusMap() {
                   )}
                 </Popup>
               </Marker>
-          );
-        }
-      })}
-      {/* </MarkerClusterGroup> */}
-
-    
-    </MapContainer>
+            );
+          }
+        })}
+        {/* </MarkerClusterGroup> */}
+      </MapContainer>
+    </>
   );
 }
