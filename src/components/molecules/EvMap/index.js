a69d8752ad6ff912305a7318f8b5b1881d1a0826
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-07-21 12:53:06 +0100

    alternative maps

--- src/components/molecules/EvMap/index.js
+++ src/components/molecules/EvMap/index.js
@@ -10,7 +10,8 @@ export default function EvMap() {
   return (
     <MapContainer center={[54.97206769445005, -1.6132124536205563]} zoom={14}>
       <TileLayer
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+        maxZoom={19}
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
       />
 
