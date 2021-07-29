80f50061d26c39486ae4284e757db6a30795b4ef
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-07-29 10:40:58 +0100

    add alt data for images

--- src/components/molecules/ColorKey/index.js
+++ src/components/molecules/ColorKey/index.js
@@ -6,16 +6,32 @@ import styles from "./ColorKey.module.css";
 export default function ColorKey() {
   return (
     <Box color="grey" fontColor="black">
-      <key className={styles.key}>
-        <StaticImage width={24} src="../../../images/carparkiconsred.png" />
+      <div className={styles.key}>
+        <StaticImage
+          alt="car park icon red Limited Spaces"
+          width={24}
+          src="../../../images/carparkiconsred.png"
+        />
         <p className={styles.keyText}>Limited Spaces</p>
-        <StaticImage width={24} src="../../../images/carparkiconsOrange.png" />
+        <StaticImage
+          alt="car park icon orange Moderatly Busy"
+          width={24}
+          src="../../../images/carparkiconsOrange.png"
+        />
         <p className={styles.keyText}>Moderatly Busy</p>
-        <StaticImage width={24} src="../../../images/carparkicons.png" />
+        <StaticImage
+          alt="car park icon green Many Spaces Available"
+          width={24}
+          src="../../../images/carparkicons.png"
+        />
         <p className={styles.keyText}>Many Spaces Available</p>
-        <StaticImage width={24} src="../../../images/carparkiconsBlack.png" />
+        <StaticImage
+          alt="car park icon black No Data"
+          width={24}
+          src="../../../images/carparkiconsBlack.png"
+        />
         <p className={styles.keyText}>No Data</p>
-      </key>
+      </div>
     </Box>
   );
 }
