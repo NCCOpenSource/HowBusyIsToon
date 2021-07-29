4824fedd2bc4e0d8a103e23422cf87dbfc429a06
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-07-29 09:39:35 +0100

    refactor Emojicon Widget add floating feedback

---
+++ src/components/molecules/FloatingFeedback/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import styles from "./FloatingFeedback.module.css";
+import EmojicomWidget from "../../molecules/EmojicomWidget";
+
+export default function FloatingFeedback() {
+  const [show, setShow] = useState(true);
+
+  const showFloat = (event) => {
+    event.preventDefault();
+    setShow(!show);
+  };
+
+  return (
+    <>
+      {show ? (
+        <div className={styles.tellUsMore}>
+          <EmojicomWidget campaignId={"7EqC031q88ez6axvEp8w"} />
+          <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=wLSfsgQNn0q0YsEpSx4bRw6915Qqm9RJjDrXRTIs2UxURFJORlJOMTFDVU03TjlWWFZFSTE2R0cwQi4u">
+            Tell Us More...
+          </a>
+          <p onClick={showFloat}>x</p>
+        </div>
+      ) : (
+        <h1>HELLO</h1>
+      )}
+    </>
+  );
+}
