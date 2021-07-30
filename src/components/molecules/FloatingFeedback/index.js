dd28580dbe6a4fb518eb961857023b03816a83c5
Sam Morgan-Tyghe <samuelmorganvisual@gmail.com>
2021-07-30 13:05:44 +0100

    hotfixes

--- src/components/molecules/FloatingFeedback/index.js
+++ src/components/molecules/FloatingFeedback/index.js
@@ -12,7 +12,7 @@ export default function FloatingFeedback() {
 
   return (
     <>
-      {show ? (
+      {isFloatShowing ? (
         <div className={styles.tellUsMore}>
           <EmojicomWidget campaignId={"7EqC031q88ez6axvEp8w"} />
           <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=wLSfsgQNn0q0YsEpSx4bRw6915Qqm9RJjDrXRTIs2UxURFJORlJOMTFDVU03TjlWWFZFSTE2R0cwQi4u">
