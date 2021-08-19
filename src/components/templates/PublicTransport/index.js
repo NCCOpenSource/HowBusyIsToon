30f87b9ed888c61f491023da7c98b61f4a9ce0f2
Alex Rossell <alex.rossell@hedgehoglab.com>
2021-08-19 10:24:27 +0100

    no message

--- src/components/templates/PublicTransport/index.js
+++ src/components/templates/PublicTransport/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import BusMap from "../../molecules/BusMap";
 import ColorKey from "../../molecules/ColorKey";
 import CouncilLinks from "../../molecules/CouncilLinks";
+import LocalTravelUpdates from "../../molecules/LocalTravelUpdates";
+import NexusArticle from "../../molecules/NexusArticle";
 import UsefulLinks from "../../organisms/UsefulLinks";
 import styles from "./PublicTransport.module.css";
+import styles2 from "../Nexus/Nexus.module.css";
 
 export default function PublicTransport() {
   return (
@@ -13,6 +16,12 @@ export default function PublicTransport() {
       </div>
       <div className={styles.BusMap}>
         <BusMap />
+      </div>
+      <div className={styles2.Innercontainer}>
+        <NexusArticle />
+        <div className={styles2.SectionExample}>
+          <LocalTravelUpdates />
+        </div>
       </div>
       <div className={styles.UsefulLinks}>
         <UsefulLinks />
