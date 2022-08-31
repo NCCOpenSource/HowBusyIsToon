9967cf1d780c9f571755213f83afd4d9e564d88a
C Gibson <43207415+nccchris@users.noreply.github.com>
2022-08-31 09:19:07 +0100

    Update index.js

--- src/components/molecules/NavLinkItems/index.js
+++ src/components/molecules/NavLinkItems/index.js
@@ -7,7 +7,8 @@ export default function NavLinkItems() {
   /* eslint-disable */
   const [path, setPath] = useState(useLocation().pathname);
   /* eslint-enable */
-
+ return(<>);
+        
   return (
     <>
       <li>
